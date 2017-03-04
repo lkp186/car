fcd1f0d77487c31b4bf04aa801be2cfc626b6d02
liukangping <1242580740@qq.com>
2017-03-04 22:23:10 +0800

    微信开发-----天气查询初步

--- public/weixin/index.php
+++ public/weixin/index.php
@@ -182,7 +182,7 @@ $item_str
                 'HQMusicUrl'=>'http://b8107.cn/public/weixin/龙登杰 - Purple Passion紫色激情（重录版）.mp3');
             $result=$this->transMusic($obj,$content);
         }elseif (strstr($keyword,'天气')){
-            $url="http://b8107.cn/weather?city=".substr($keyword,1);
+            $url="http://b8107.cn/weather?city=".substr($keyword,2);
             $json=$this->http_request($url);
             $attr=json_decode($json,true);
             if(empty($attr)){
@@ -192,7 +192,7 @@ $item_str
                 if($attr['status']==0){
                     $content="结果正常";$result=$this->transText($obj,$content);
                 }else{
-                    $content=$keyword."发生了位置的错误";
+                    $content="发生了位置的错误".substr($keyword,2);
                     $result=$this->transText($obj,$content);
                 }
             }
