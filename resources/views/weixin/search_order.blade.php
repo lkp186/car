4286351432e77f8747684cea84e60c0fbc2f5b47
Administrator <1242580740@qq.com>
2017-03-08 10:59:54 +0800

    订单查询

--- resources/views/weixin/search_order.blade.php
+++ resources/views/weixin/search_order.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>查询结果</title>
     <link rel="stylesheet" href="{{asset('public/css/bootstrap.min.css')}}">
 </head>
