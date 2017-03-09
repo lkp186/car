8d81ad8df052de1562b475e81e1aab8cd30856f2
Administrator <1242580740@qq.com>
2017-03-09 13:42:14 +0800

    新人指引

--- resources/views/weixin/help.blade.php
+++ resources/views/weixin/help.blade.php
@@ -7,7 +7,7 @@
 <div class="row">
     <div class="col-sm-6 col-md-4">
         <div class="thumbnail">
-            <img src="{{'public/weixin/image/1.jpg'}}" alt="">
+            <img src="{{asset('public/weixin/image/1.jpg')}}" alt="">
             <div class="caption">
                 <h3>Thumbnail label</h3>
                 <p>...</p>
@@ -16,7 +16,7 @@
     </div>
     <div class="col-sm-6 col-md-4">
         <div class="thumbnail">
-            <img src="{{'public/weixin/image/2.jpg'}}" alt="">
+            <img src="{{asset('public/weixin/image/2.jpg')}}" alt="">
             <div class="caption">
                 <h3>Thumbnail label</h3>
                 <p>...</p>
@@ -25,7 +25,7 @@
     </div>
     <div class="col-sm-6 col-md-4">
         <div class="thumbnail">
-            <img src="{{'public/weixin/image/3.jpg'}}" alt="">
+            <img src="{{asset('public/weixin/image/3.jpg')}}" alt="">
             <div class="caption">
                 <h3>Thumbnail label</h3>
                 <p>...</p>
