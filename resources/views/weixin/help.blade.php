864af5432b7c5426dd930f05237e1054dfa8b238
Administrator <1242580740@qq.com>
2017-03-09 14:29:37 +0800

    新人指引

--- resources/views/weixin/help.blade.php
+++ resources/views/weixin/help.blade.php
@@ -2,6 +2,7 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title></title>
+    <link rel="stylesheet" href="{{asset('public/css/bootstrap.min.css')}}">
 </head>
 <body>
 <div class="row" align="center">
