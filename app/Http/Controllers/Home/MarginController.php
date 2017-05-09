43e9a9cb694b77b03a10ef0dc739f4bf93c9e8b7
liukangping <1242580740@qq.com>
2017-05-09 10:13:36 +0800

    2017-5-9  修复了缴纳保证金的bug

--- app/Http/Controllers/Home/MarginController.php
+++ app/Http/Controllers/Home/MarginController.php
@@ -19,14 +19,21 @@ class MarginController extends Controller
     //缴纳保证金的操作
     public function payMargin(Request $request){
         $ID=$request->input('ID');
+        //根据用户的身份证号查出用户的姓名
         $user_name=User_info::where('user_ID_card',$ID)->value('user_name');
-        $margin=new Margin_info;
-        $margin->margin_name=$user_name;
-        $margin->margin_ID_card=$ID;
-        $margin->margin_status='1';
-        $margin->margin_number='3000';
-        $margin->margin_balance='3000';
-        $margin->save();
+        $margin_name=Margin_info::where('margin_ID_card',$ID)->value('margin_name');
+        if(empty($margin_name)){
+            $margin=new Margin_info;
+            $margin->margin_name=$user_name;
+            $margin->margin_ID_card=$ID;
+            $margin->margin_status='1';
+            $margin->margin_number='3000';
+            $margin->margin_balance='3000';
+            $margin->save();
+        }else{
+            $margin_balance=Margin_info::where('margin_ID_card',$ID)->value('margin_balance');
+            Margin_info::where('margin_ID_card',$ID)->update(['margin_balance'=>$margin_balance+3000]);
+        }
         return $ID;
     }
 }
