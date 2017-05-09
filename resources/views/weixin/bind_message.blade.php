034403d12aa35eba68ac98647d4f77cf873d57ce
liukangping <1242580740@qq.com>
2017-05-09 17:41:11 +0800

    2017-5-9  修改了微信的自定义菜单，微信账号绑定功能

--- resources/views/weixin/bind_message.blade.php
+++ resources/views/weixin/bind_message.blade.php
@@ -5,8 +5,15 @@
     <link rel="stylesheet" href="{{asset('public/css/bootstrap.min.css')}}">
 </head>
 <body>
-<div>
-{{$msg}}
+<div class="container">
+    <div align="center">
+        @if($msg='绑定成功')
+            <img src="{{asset('public/weixin/sign-check-icon.png')}}">{{$msg}}
+        @else
+            <img src="{{asset('public/weixin/sign-error-icon.png')}}">{{$msg}}
+        @endif
+    </div>
+
 </div>
 </body>
 </html>
