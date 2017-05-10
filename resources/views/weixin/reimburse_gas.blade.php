05dd935b823dae5a1349fe1b2c7b67104cf317ca
liukangping <1242580740@qq.com>
2017-05-10 09:28:19 +0800

    2017-5-9  邮费报销功能，图片上传部分

--- resources/views/weixin/reimburse_gas.blade.php
+++ resources/views/weixin/reimburse_gas.blade.php
@@ -4,7 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>油费报销</title>
     <link rel="stylesheet" href="{{asset('public/css/bootstrap.min.css')}}">
-    <link rel="stylesheet" href="{{asset('public/css/fileinput.min.css')}}">
+    <link rel="stylesheet" href="{{asset('public/weixin/css/fileinput.min.css')}}">
     <script type="text/javascript" src="{{asset('public/weixin/js/fileinput.min.js')}}"></script>
 </head>
 <body>
@@ -12,7 +12,7 @@
 <div class="container">
     <form class="form-horizontal" action="{{url('weiChat/reimburse/opt')}}" method="post" enctype="multipart/form-data">
         {{csrf_field()}}
-        <input type="hidden" value="{{$OpenID}}">
+        <input type="hidden" value="{{$OpenID}}" name="OpenID">
         <div class="form-group">
             <label class="col-sm-2 control-label">小贴士:</label>
             <label class="col-sm-7 control-label">报销需要加油发票照片以及车辆油表在加油前后的油表照片</label>
@@ -20,7 +20,7 @@
         <div class="form-group">
             <label for="inputEmail3" class="col-sm-2 control-label">加油发票图片</label>
             <div class="col-sm-10">
-                <input type="file" name="gas_invoice" class="form-control" id="gas_invoice" placeholder="加油发票">
+                <input type="file" name="gas_invoice" class="file form-control" id="gas_invoice" placeholder="加油发票">
             </div>
         </div>
         <div class="form-group">
