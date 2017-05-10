3610733dec7130ae143d9c66730489c379677807
liukangping <1242580740@qq.com>
2017-05-10 16:10:24 +0800

    2017-5-9 完成了微信公众号，新人指引

--- resources/views/weixin/help.blade.php
+++ resources/views/weixin/help.blade.php
@@ -10,8 +10,10 @@
         <div class="thumbnail">
             <img src="{{asset('public/weixin/image/nilu.jpg')}}" alt="">
             <div class="caption">
-                <h3>Thumbnail label</h3>
-                <p>...</p>
+                <h3>拯救你的钱包</h3>
+                <p>您用车，我供车。比起私家车，
+                    省去维修、保养、保险、折旧等一系列大支出；比起出租车，我们的油费更少水分。
+                    经济实惠，让您的钱包鼓一点</p>
             </div>
         </div>
     </div>
@@ -19,8 +21,8 @@
         <div class="thumbnail">
             <img src="{{asset('public/weixin/image/2.jpg')}}" alt="">
             <div class="caption">
-                <h3>Thumbnail label</h3>
-                <p>...</p>
+                <h3>不再寂寞的等待</h3>
+                <p>网点分布众多，充分考虑群众需求,打车公交都是浮云！妈妈再也不用担心我赶不上晚饭啦！</p>
             </div>
         </div>
     </div>
@@ -28,8 +30,8 @@
         <div class="thumbnail">
             <img src="{{asset('public/weixin/image/3.jpg')}}" alt="">
             <div class="caption">
-                <h3>Thumbnail label</h3>
-                <p>...</p>
+                <h3>终结养车烦恼</h3>
+                <p>网上注册，多元化的支付手段。尽最大的努力为您提供服务。车纷享，您口袋里的私家车。</p>
             </div>
         </div>
     </div>
